0b044802b3e483ab50d18392f377090d837e48b8
Ginsway <ginsway@qq.com>
2022-09-11 11:30:48 +0800

    调整

--- src/main/java/com/ginsway/dontstarve/mixin/DontStarve3Mixin.java
+++ src/main/java/com/ginsway/dontstarve/mixin/DontStarve3Mixin.java
@@ -1,6 +1,6 @@
 package com.ginsway.dontstarve.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.ginsway.dontstarve.DontStarve3;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class DontStarve3Mixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		DontStarve3.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
