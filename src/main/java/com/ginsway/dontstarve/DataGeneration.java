4d982a41fb469e6344ba0037fca823317acc6a1d
Ginsway <ginsway@qq.com>
2023-01-11 11:55:16 +0800

    datagen

---
+++ src/main/java/com/ginsway/dontstarve/DataGeneration.java
@@ -0,0 +1,11 @@
+package com.ginsway.dontstarve;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGeneration implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
